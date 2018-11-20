5726f34ee89c20c599df7f1c91b117f979fb07c8
JoJo <apple@apples-MacBook-Pro.local>
2018-11-20 12:59:48 -0800

    JS Drills version 1

---
+++ app.js
@@ -0,0 +1,42 @@
+// setName();
+// function setName(){
+//     var name = 'Joseph';
+//     console.log(name);
+// } 
+
+
+// console.log('some text');
+// let avg = findAvg(2, 2);
+// console.log('some text', avg);
+// function findAvg(a, b) {
+//     console.log('some text');
+//     var answer = ( a + b) / 2;
+//     return answer;
+// }
+
+// let fruits = ['apples', 'pears', 'kiwi'];
+
+// function printFruits(){
+//     let favFruit = fruits[1];
+//     console.log(fruits[0]);
+//     printFavFruit();
+//     console.log(leastFav);
+
+//     function printFavFruit(){
+//         console.log(favFruit);
+//         let leastFav = fruits[2];
+//     }
+// }
+
+// printFruits();
+
+newFunc();
+function newFunc(){
+    console.log('Hello');
+}
+
+
+let secondFunc = function(){
+    alert('Test');
+}
+secondFunc();
